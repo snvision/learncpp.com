4f452f3da68ea3119a18dd14195b754f237bbc1a
Савченков Никита <savchienkov.nikita@gmail.com>
2024-07-27 15:26:32 +0300

    end write algorithm

--- sumAndDiffTwoNum/main.cpp
+++ sumAndDiffTwoNum/main.cpp
@@ -1,7 +1,29 @@
+#include <cstdint>
 #include <cstdlib>
+#include <iostream>
+
+
+// Считывание числа с клавиатуры
+std::int16_t getNumber() {
+  std::int16_t number;
+  std::cin >> number;
+  return number;
+}
+
+// Печать суммы и разности чисел, введённых с клавиатуры.
+void printSumAndDiffNumber(std::int16_t firstNumber, std::int16_t secondNumber) {
+  std::cout << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << '.' << '\n'
+            << firstNumber << " - " << secondNumber << " is " << firstNumber - secondNumber << '.' << '\n';
+}
 
 
 int main() {
+  std::cout << "Enter first integer number: ";
+  std::int16_t firstNumber = getNumber();
+  std::cout << "Enter second integer number: ";
+  std::int16_t secondNumber = getNumber();
+
+  printSumAndDiffNumber(firstNumber, secondNumber);
 
   return EXIT_SUCCESS;
 }
