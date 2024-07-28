3dfe5502af86090b37b582e8a82d9b30fe604f36
Савченков Никита <savchienkov.nikita@gmail.com>
2024-07-28 23:13:50 +0300

    refactoring code
    Упростил читаемость, добавил константность для операндов сложения и вычитания.

--- sumAndDiffTwoNum/main.cpp
+++ sumAndDiffTwoNum/main.cpp
@@ -10,20 +10,15 @@ std::int16_t getNumber() {
   return number;
 }
 
-// Печать суммы и разности чисел, введённых с клавиатуры.
-void printSumAndDiffNumber(std::int16_t firstNumber, std::int16_t secondNumber) {
-  std::cout << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << '.' << '\n'
-            << firstNumber << " - " << secondNumber << " is " << firstNumber - secondNumber << '.' << '\n';
-}
-
-
 int main() {
   std::cout << "Enter first integer number: ";
-  std::int16_t firstNumber = getNumber();
+  const std::int16_t firstNumber = getNumber();
   std::cout << "Enter second integer number: ";
-  std::int16_t secondNumber = getNumber();
+  const std::int16_t secondNumber = getNumber();
 
-  printSumAndDiffNumber(firstNumber, secondNumber);
+  // Печать суммы и разности чисел, введённых с клавиатуры.
+  std::cout << firstNumber << " + " << secondNumber << " is " << firstNumber + secondNumber << '.' << '\n'
+            << firstNumber << " - " << secondNumber << " is " << firstNumber - secondNumber << '.' << '\n';
 
   return EXIT_SUCCESS;
 }
