a48b36046d140d46a1f17f5bb4f8d5e33cf59120
Савченков Никита <savchienkov.nikita@gmail.com>
2024-08-05 22:43:17 +0300

    refactoring code
    Добавил пустую строку между функциями и добавил скобки при печати результата выражения

--- centuryFromYear/main.cpp
+++ centuryFromYear/main.cpp
@@ -11,12 +11,13 @@ std::int16_t getYear() {
   return year;
 }
 
+
 int main() {
   const std::int16_t year = getYear();
 
   // К году прибавляется 99 и затем сумма делится на 100,
   // чтобы получить номер века.
-  std::cout << "Century: " << (year + 99) / 100 << '\n';
+  std::cout << "Century: " << ((year + 99) / 100) << '\n';
 
   return EXIT_SUCCESS;
 }
