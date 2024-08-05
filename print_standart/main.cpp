52e3dca683a502b9233a655461fa0eed1826b30a
Савченков Никита <savchienkov.nikita@gmail.com>
2024-08-05 22:37:01 +0300

    refactoring code

--- print_standart/main.cpp
+++ print_standart/main.cpp
@@ -5,17 +5,16 @@
 
 const int numStandards = 7;
 // The C++26 stdCode is a placeholder since the exact code won't be determined until the standard is finalized
-const long stdCode[numStandards] = { 199711L, 201103L, 201402L, 201703L, 202002L, 202302L, 202612L};
-const char* stdName[numStandards] = { "Pre-C++11", "C++11", "C++14", "C++17", "C++20", "C++23", "C++26" };
+const long  stdCode[numStandards] = {199711L, 201103L, 201402L, 201703L, 202002L, 202302L, 202612L};
+const char* stdName[numStandards] = {"Pre-C++11", "C++11", "C++14", "C++17", "C++20", "C++23", "C++26"};
 
-long getCPPStandard()
-{
+long getCPPStandard() {
   // Visual Studio is non-conforming in support for __cplusplus (unless you set a specific compiler flag, which you probably haven't)
   // In Visual Studio к or newer we can use _MSVC_LANG instead
   // See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
-#if defined (_MSVC_LANG)
+#if defined(_MSVC_LANG)
   return _MSVC_LANG;
-#elif defined (_MSC_VER)
+#elif defined(_MSC_VER)
   // If we're using an older version of Visual Studio, bail out
   return -1;
 #else
@@ -24,22 +23,18 @@ long getCPPStandard()
 #endif
 }
 
-int main()
-{
-  long standard = getCPPStandard();
+int main() {
+  long const standard = getCPPStandard();
 
-  if (standard == -1)
-  {
+  if (standard == -1) {
     std::cout << "Error: Unable to determine your language standard.  Sorry.\n";
-    return 0;
+    return EXIT_SUCCESS;
   }
 
-  for (int i = 0; i < numStandards; ++i)
-  {
+  for (auto i = 0z; i < numStandards; ++i) {
     // If the reported version is one of the finalized standard codes
     // then we know exactly what version the compiler is running
-    if (standard == stdCode[i])
-    {
+    if (standard == stdCode[i]) {
       std::cout << "Your compiler is using " << stdName[i]
                 << " (language standard code " << standard << "L)\n";
       break;
@@ -47,13 +42,12 @@ int main()
 
     // If the reported version is between two finalized standard codes,
     // this must be a preview / experimental support for the next upcoming version.
-    if (standard < stdCode[i])
-    {
+    if (standard < stdCode[i]) {
       std::cout << "Your compiler is using a preview/pre-release of " << stdName[i]
                 << " (language standard code " << standard << "L)\n";
       break;
     }
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
